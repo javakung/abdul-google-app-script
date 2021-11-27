bdf9bdf52d2b855e8cf8385ca6e919b1dcc18515
javakung <javakung@users.noreply.github.com>
2021-11-27 13:00:43 +0700

    Create getsheet2json.js

---
+++ getsheet2json.js
@@ -0,0 +1,15 @@
+function getContent(){
+  var sheet = SpreadsheetApp.openById('[SHEETID]')
+  var values = sheet.getDataRange().getDisplayValues();
+  var header = values[0];
+  
+  var data = [];
+  for(var i = 1;i<values.length;i++){
+    var u = values[i];
+    var item = {};
+    for(var x = 0;x<u.length;x++)item[header[x]]=u[x];
+    data.push(item);
+  } 
+  return ContentService.createTextOutput(JSON.stringify(data))
+  .setMimeType(ContentService.MimeType.JSON)
+}
